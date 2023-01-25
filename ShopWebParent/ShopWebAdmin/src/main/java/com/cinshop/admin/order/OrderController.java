fd430defe5485d0fb8f3e755da5fdf448180f3fe
chien <chient369@gmail.com>
2023-01-25 12:48:02 +0900

    code order detail page

--- ShopWebParent/ShopWebAdmin/src/main/java/com/cinshop/admin/order/OrderController.java
+++ ShopWebParent/ShopWebAdmin/src/main/java/com/cinshop/admin/order/OrderController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.cinshop.common.OrderStatusUtility;
+import com.cinshop.common.entity.Order;
+
 @Controller
 public class OrderController {
 
@@ -18,4 +21,13 @@ public class OrderController {
 		return "orderDetail";
 	}
 	
+	@GetMapping("/order/u/{oId}/{sts}")
+	public String updateOrderStatus(Model model,@PathVariable("oId") Integer oId, @PathVariable("sts") Integer sts) {
+		Order order = orderService.findOrderById(oId);
+		order.setStatus(OrderStatusUtility.getOrderStatus(sts));
+		System.out.println(order.getStatus().name());
+		Order updatedOrder = orderService.updateOrder(order);
+		model.addAttribute("order",updatedOrder);
+		return "orderDetail";
+	}
 }
